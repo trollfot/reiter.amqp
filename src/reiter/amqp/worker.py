4022cbe71fb08a4de891d55d0d8466211f4ec783
Souheil CHELFOUH <trollfot@gmail.com>
2021-06-08 17:52:16 +0200

    Changed the Worker signature

--- src/reiter/amqp/worker.py
+++ src/reiter/amqp/worker.py
@@ -10,26 +10,23 @@ class Worker(ConsumerMixin):
 
     connection: Optional[Connection] = None
 
-    def __init__(self, config, amqpcenter: AMQPCenter = AMQP):
-        self.context = {}
-        self.config = config
+    def __init__(self, amqpcenter: AMQPCenter, url, app):
+        self.url = url
+        self.app = app
         self.amqpcenter = amqpcenter
         self.thread = threading.Thread(target=self.__call__)
 
     def get_consumers(self, Consumer, channel):
-        consumers = list(
-            self.amqpcenter.consumers(Consumer, channel, **self.context))
-        return consumers
+        return list(
+            self.amqpcenter.consumers(Consumer, channel, app=self.app))
 
-    def __call__(self, **context):
+    def __call__(self):
         try:
-            self.context = context
-            with Connection(self.config.url) as conn:
+            with Connection(self.url) as conn:
                 self.connection = conn
                 self.run()
         finally:
             self.connection = None
-            self.context = None
 
     def start(self):
         self.thread.start()
